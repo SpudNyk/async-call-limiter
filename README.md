f4e76d6c9e21a6c896ca55bf05a50092990b4df3
Nick Channon <spudnyk@gmail.com>
2019-11-26 17:34:20 -0500

    Preparing for publish

--- README.md
+++ README.md
@@ -2,11 +2,15 @@
 
 A set of functions useful in an async/await environment.
 
+## Description
+
+These functions are similar to others debounce and throttle functions. e.g. [Lodash's](https://lodash.com): [debounce](https://lodash.com/docs/4.17.15#debounce) and [throttle](https://lodash.com/docs/4.17.15#throttle).
+However these functions differ in that the resulting function when called returns a promise that will resolve/reject when the wrapped function is invoked. The functions also can take an "arugment reducer" function that determines the arguments that will be given to wrapped function.
+The default reducer implementation just uses the last arguments given, however you can use this to merge arguments so the wrapped function can handle all calls. This could be useful wrapping a request for specific items to merge multiple calls into one request.
+  
 ## Functions
 
-  Debounce and throttle can take a reducer function to merge multiple calls into a single set of arguments.
-  
-  See source for details.
+There are some JSDoc comments in [the source](https://github.com/SpudNyk/async-call-limiter/tree/master/es) for more information on arguments. The `es` folder contains the source code.
 
   - `debounce`
   - `throttle`
---
+++ babel.config.js
@@ -0,0 +1,49 @@
+module.exports = api => {
+    const isTest = api.env('test');
+
+    return {
+        ignore: isTest
+            ? undefined
+            : [
+                  '**/__tests__', // ignore the whole test directory
+                  '**/*.test.js' // ignore test files only
+              ],
+        env: {
+            test: {
+                plugins: ['@babel/transform-modules-commonjs']
+            },
+            cjs: {
+                presets: [
+                    [
+                        '@babel/preset-env',
+                        {
+                            modules: 'commonjs',
+                            targets: {
+                                node: '10'
+                            }
+                        }
+                    ]
+                ]
+            },
+            esm: {
+                plugins: [
+                    ['@babel/plugin-transform-runtime', { useESModules: true }]
+                ],
+                presets: [
+                    [
+                        '@babel/preset-env',
+                        {
+                            modules: false,
+                            targets: {
+                                esmodules: true
+                            }
+                        }
+                    ]
+                ]
+            },
+            es: {
+
+            }
+        }
+    };
+};
